85b452b80e22ad74389abef8356cf85318ab819a
rcon <reg710@gmail.com>
2021-11-07 13:23:14 -0500

    Modified for /favicon.ico error. Chapt 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -10,6 +10,10 @@ module Rulers
   # class Error < StandardError; end
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
