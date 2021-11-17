d3a89e740d9bf4ee80b4d013b1b80528e49646b8
rcon <reg710@gmail.com>
2021-11-16 20:55:59 -0500

    Added util.rb file which converts CamelCase to snake_case

---
+++ lib/rulers/util.rb
@@ -0,0 +1,10 @@
+# This converts CamelCase controller names to snake_case file names
+
+module Rulers
+    def self.to_underscore(string)
+      string.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+downcase end
+end
